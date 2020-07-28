666f5b6f0e433fde14ef9377bdfb453b4c254a2b
Sima <sima.boreiri@gmail.com>
2020-07-28 17:26:40 +0200

    adding first feature test to the app

---
+++ cypress/integration/userCanPlayRpsGame.feature.js
@@ -0,0 +1,10 @@
+describe(' user can play RPS game', () => {
+  before(() => {
+    cy.visit('/');
+  })
+  it('user can start to play RPS game', () => {
+   cy.get('input#name').type('Sima')
+   cy.get('button#play').click();
+   cy.get('p#welcome-message').should('contain', ' Welcome Sima, Start RPS game by clicking on one of the icons')
+  })
+})
